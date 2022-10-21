35b384c735349e99cdb136d71bae5c81ebba0a27
Terence <ftngwes@gmail.com>
2022-10-21 00:11:07 -0400

    my content3

--- fourty.sh
+++ fourty.sh
@@ -1,2 +1,4 @@
 When i become determine to learn something, i do my whole best to become master at it.
 I want to surprise my family with my achievement from this devops program.
+Mama Rose and my wife Beny you guys will be amaze for what i will become in future.
+may God be my helper.
